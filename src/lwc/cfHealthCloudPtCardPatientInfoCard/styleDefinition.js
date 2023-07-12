cd9da91913eb46aa81873164dff80047db8939f1
JanarthananJay <jjanarthanan@salesforce.com>
2023-07-12 15:07:13 -0500

    Checking in Src

---
+++ src/lwc/cfHealthCloudPtCardPatientInfoCard/styleDefinition.js
@@ -0,0 +1,3 @@
+let styleDefinition =
+      {"state0element2block_element0":[{"conditions":"default","styleObject":{"class":"slds-col  condition-element  slds-size_8-of-12  ","style":"","styleProperties":""}}]};
+  export default styleDefinition
